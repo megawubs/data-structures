2d82e2da77ee21c133b32ce7136f1383169583dc
Bram Wubs <b.wubs@code14.nl>
2017-03-25 13:45:17 +0100

    I'm proud of my heap

--- DataStructures/DataStructures/Structures/Heap.cs
+++ DataStructures/DataStructures/Structures/Heap.cs
@@ -10,20 +10,46 @@ namespace DataStructures.Structures
 
     public class Heap<T> where T : IComparable<T>
     {
-        private T[] _items = new T[50];
+        private T[] _items;
         private int _currentSize;
         private HeapType _type;
 
         public int Length => _currentSize;
 
-        public Heap(HeapType type)
+        public Heap(int capacity, HeapType type)
         {
             _type = type;
+            _items = new T[capacity];
+        }
+
+        public Heap() : this(50, HeapType.Min)
+        {
+
+        }
+
+        public Heap(int capacity) : this(capacity, HeapType.Min)
+        {
+
+        }
+
+        public Heap(HeapType type) : this(50, type)
+        {
+
+        }
+
+        public Heap(T[] items, HeapType type, int capacity) : this(capacity, type)
+        {
+            BuildHeap(items);
         }
 
         public void Add(T data)
         {
-            int hole = ++_currentSize;
+            if (_currentSize == _items.Length - 1)
+            {
+                Resize();
+            }
+            _currentSize = _currentSize + 1;
+            int hole = _currentSize;
             for (; Compare(data, _items[hole / 2]) < 0 && hole > 1; hole /= 2)
             {
                 _items[hole] = _items[hole / 2];
@@ -36,10 +62,13 @@ namespace DataStructures.Structures
             T value = _items[1];
             T left = _items[2 * 1];
             T right = _items[2 * 1 + 1];
-            T data = _items[_currentSize--];
+            T data = _items[_currentSize];
+            _currentSize = _currentSize - 1;
             int hole = 1;
+            // reorder stuff when there are more than 1 items
             if (_currentSize > 1)
             {
+                //is left smaller than right? go left.
                 if (Compare(left, right) < 0)
                 {
                     for (; Compare(data, _items[hole * 2]) > 0 && hole * 2 < _currentSize; hole *= 2)
@@ -47,6 +76,7 @@ namespace DataStructures.Structures
                         _items[hole] = _items[hole * 2];
                     }
                 }
+                //if left is bigger than right, go right
                 else
                 {
                     for (; Compare(data, _items[hole * 2 + 1]) > 0 && hole * 2 + 1<_currentSize ; hole *= 2 + 1)
@@ -55,7 +85,7 @@ namespace DataStructures.Structures
                     }
                 }
             }
-
+            //set the data
             _items[hole] = data;
             return value;
         }
@@ -67,21 +97,21 @@ namespace DataStructures.Structures
 
         private int Compare(T input, T input2)
         {
+            //when using HeapType.Max input2 can be null, check this first.
             if (input2 == null)
             {
                 return 0;
             }
 
+            //I know it works, but can't explain
+            //has to do something with heaps and types
+            //and compare and stuff
             if (_type == HeapType.Min)
             {
                 return input.CompareTo(input2);
             }
-            if (_type == HeapType.Max)
-            {
-                return input2.CompareTo(input);
-            }
-
-            throw new ArgumentException("A heap type is required");
+            //assume HeapType.Max
+            return input2.CompareTo(input);
         }
 
         public override string ToString()
@@ -92,6 +122,37 @@ namespace DataStructures.Structures
                 str += $"[ {_items[i]} ], ";
             }
             return str;
+        }
+
+        private void Resize()
+        {
+            var newList = new T[_items.Length * 2];
+            for (var i = 0; i < _items.Length; i++)
+            {
+                newList[i] = _items[i];
+            }
+
+            _items = newList;
+        }
+
+        public Heap<T> Invert()
+        {
+            var type = _type == HeapType.Max ? HeapType.Min : HeapType.Max;
+
+            var items = new T[_currentSize];
+            for (int i = 1; i <= _currentSize; i++)
+            {
+                items[i -1 ] = _items[i];
+            }
+            return new Heap<T>(items, type, _items.Length);
+        }
+
+        public void BuildHeap(T[] items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Add(items[i]);
+            }
         }
     }
 }
--- DataStructures/DataStructuresTest/Structures/HeapTest.cs
+++ DataStructures/DataStructuresTest/Structures/HeapTest.cs
@@ -1,4 +1,5 @@
-﻿using DataStructures.Structures;
+﻿using System.Runtime.InteropServices.ComTypes;
+using DataStructures.Structures;
 using NUnit.Framework;
 
 namespace DataStructuresTest.Structures
@@ -9,7 +10,7 @@ namespace DataStructuresTest.Structures
         [Test]
         public void max_heap_works_with_strings()
         {
-            var heap = new Heap<string>(HeapType.Max);
+            var heap = new Heap<string>(10, HeapType.Max);
 
             heap.Add("A");
             heap.Add("B");
@@ -29,7 +30,7 @@ namespace DataStructuresTest.Structures
         [Test]
         public void min_heap_works_with_strings()
         {
-            var heap = new Heap<string>(HeapType.Min);
+            var heap = new Heap<string>(10, HeapType.Min);
 
             heap.Add("F");
             heap.Add("E");
@@ -49,7 +50,7 @@ namespace DataStructuresTest.Structures
         [Test]
         public void min_heap_works_with_ints()
         {
-            var heap = new Heap<int>(HeapType.Min);
+            var heap = new Heap<int>(10);
 
             heap.Add(6);
             heap.Add(5);
@@ -57,20 +58,26 @@ namespace DataStructuresTest.Structures
             heap.Add(3);
             heap.Add(2);
             heap.Add(1);
+            heap.Add(0);
+            heap.Add(-1);
 
+            Assert.AreEqual(-1, heap.RemoveFirst());
+            Assert.AreEqual(0, heap.RemoveFirst());
             Assert.AreEqual(1, heap.RemoveFirst());
             Assert.AreEqual(2, heap.RemoveFirst());
             Assert.AreEqual(3, heap.RemoveFirst());
             Assert.AreEqual(4, heap.RemoveFirst());
             Assert.AreEqual(5, heap.RemoveFirst());
             Assert.AreEqual(6, heap.RemoveFirst());
+
         }
 
         [Test]
         public void max_heap_works_with_ints()
         {
-            var heap = new Heap<int>(HeapType.Max);
+            var heap = new Heap<int>(10, HeapType.Max);
 
+            heap.Add(0);
             heap.Add(1);
             heap.Add(2);
             heap.Add(3);
@@ -84,6 +91,88 @@ namespace DataStructuresTest.Structures
             Assert.AreEqual(3, heap.RemoveFirst());
             Assert.AreEqual(2, heap.RemoveFirst());
             Assert.AreEqual(1, heap.RemoveFirst());
-            }
+            Assert.AreEqual(0, heap.RemoveFirst());
+        }
+
+        [Test]
+        public void random_adding_and_removing_keeps_the_heap_in_tact()
+        {
+            var heap = new Heap<int>(10, HeapType.Min);
+
+            heap.Add(0);
+            Assert.AreEqual(0, heap.RemoveFirst());
+            heap.Add(5);
+            heap.Add(2);
+            Assert.AreEqual(2, heap.RemoveFirst());
+            heap.Add(10);
+            heap.Add(4);
+            heap.Add(49);
+            Assert.AreEqual(4, heap.RemoveFirst());
+            heap.Add(1);
+            Assert.AreEqual(1, heap.RemoveFirst());
+            Assert.AreEqual(3, heap.Length);
+        }
+
+        [Test]
+        public void min_heap_can_make_an_max_heap_from_itself()
+        {
+            var heap = new Heap<int>(HeapType.Min);
+            heap.Add(5);
+            heap.Add(100);
+            heap.Add(19);
+            heap.Add(3);
+            heap.Add(7);
+            heap.Add(2);
+
+            var maxHeap = heap.Invert();
+            Assert.AreEqual(100, maxHeap.RemoveFirst());
+            Assert.AreEqual(19, maxHeap.RemoveFirst());
+            Assert.AreEqual(7, maxHeap.RemoveFirst());
+            Assert.AreEqual(5, maxHeap.RemoveFirst());
+            Assert.AreEqual(3, maxHeap.RemoveFirst());
+            Assert.AreEqual(2, maxHeap.RemoveFirst());
+            Assert.AreEqual(0, maxHeap.Length);
+        }
+
+        [Test]
+        public void max_heap_can_make_an_min_heap_from_itself()
+        {
+            var heap = new Heap<int>(HeapType.Max);
+            heap.Add(5);
+            heap.Add(100);
+            heap.Add(19);
+            heap.Add(3);
+            heap.Add(7);
+            heap.Add(2);
+
+            var maxHeap = heap.Invert();
+            Assert.AreEqual(2, maxHeap.RemoveFirst());
+            Assert.AreEqual(3, maxHeap.RemoveFirst());
+            Assert.AreEqual(5, maxHeap.RemoveFirst());
+            Assert.AreEqual(7, maxHeap.RemoveFirst());
+            Assert.AreEqual(19, maxHeap.RemoveFirst());
+            Assert.AreEqual(100, maxHeap.RemoveFirst());
+        }
+
+        [Test]
+        public void resize_works()
+        {
+            var heap = new Heap<int>(2);
+            heap.Add(1);
+            heap.Add(2);
+            //rezise
+            heap.Add(3);
+            heap.Add(4);
+            //resize
+            heap.Add(5);
+            heap.Add(6);
+            heap.Add(7);
+            heap.Add(8);
+            //rezise
+            heap.Add(9);
+            heap.Add(10);
+
+            Assert.AreEqual(10, heap.Length);
+        }
     }
 }
